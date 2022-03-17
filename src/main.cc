cb971a8fbe7f5e0c9aff36d4713ff314d8db14b3
Russel Arbore <russel.jma@gmail.com>
2022-03-16 17:19:19 -0700

    Create a vulkan instance

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,51 @@
+#include <vulkan/vulkan.h>
+
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
+#define VK_ASSERT(res) {				\
+	if ((res) != VK_SUCCESS) {			\
+	    throw std::runtime_error("Vulkan failure");	\
+	}						\
+    }
+
 int main() {
-    std::cout << "vulkan-tutorial" << std::endl;
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    auto window = glfwCreateWindow(800, 600, "vulkan-tutorial", nullptr, nullptr);
+
+    VkApplicationInfo app_info;
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "vulkan-tutorial";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = "Custom";
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_3;
+
+    uint32_t glfw_ext_count = 0;
+    const char** glfw_exts = glfwGetRequiredInstanceExtensions(&glfw_ext_count);
+    VkInstanceCreateInfo create_info;
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    create_info.pApplicationInfo = &app_info;
+    create_info.enabledExtensionCount = glfw_ext_count;
+    create_info.ppEnabledExtensionNames = glfw_exts;
+    create_info.enabledLayerCount = 0;
+
+    VkInstance instance;
+    VK_ASSERT(vkCreateInstance(&create_info, nullptr, &instance));
+
+    while (!glfwWindowShouldClose(window)) {
+	glfwPollEvents();
+    }
+
+    vkDestroyInstance(instance, nullptr);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
     return 0;
 }
