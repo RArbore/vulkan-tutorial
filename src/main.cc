3c92504ea17b0fa05deb3c20dafa31631976a78b
Russel Arbore <russel.jma@gmail.com>
2022-03-17 08:59:09 -0700

    Check for validation layers

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,25 @@
-#include <vulkan/vulkan.h>
-
-#include <GLFW/glfw3.h>
-
 #include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 
 #define VK_ASSERT(res) {				\
 	if ((res) != VK_SUCCESS) {			\
 	    throw std::runtime_error("Vulkan failure");	\
 	}						\
     }
+
+const std::vector<const char*> validation_layers = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+#ifdef NDEBUG
+const bool enable_validation_layers = false;
+#else
+const bool enable_validation_layers = true;
+#endif
 
 int main() {
     glfwInit();
@@ -17,6 +28,26 @@ int main() {
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     auto window = glfwCreateWindow(800, 600, "vulkan-tutorial", nullptr, nullptr);
+
+    if (enable_validation_layers) {
+	uint32_t validation_layer_count = 0;
+	vkEnumerateInstanceLayerProperties(&validation_layer_count, nullptr);
+	VkLayerProperties *available_validation_layers = new VkLayerProperties[validation_layer_count];
+	vkEnumerateInstanceLayerProperties(&validation_layer_count, available_validation_layers);
+	for (const char* layer_name : validation_layers) {
+	    std::size_t i = 0;
+	    for (; i < validation_layer_count; ++i) {
+		auto layer_props = available_validation_layers[i];
+		if (!strcmp(layer_name, layer_props.layerName)) {
+		    break;
+		}
+	    }
+
+	    if (i >= validation_layer_count) {
+		VK_ASSERT(VK_ERROR_VALIDATION_FAILED_EXT)
+	    }
+	}
+    }
 
     VkApplicationInfo app_info;
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
